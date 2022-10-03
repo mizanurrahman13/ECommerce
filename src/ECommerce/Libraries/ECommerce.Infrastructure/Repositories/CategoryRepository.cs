cff88c38158bbfdee0c37d2c4fd46641b73d9993
mizanurrahman13 <mizan.441980@gmail.com>
2022-10-03 13:26:19 +0600

    Category Repository Added

---
+++ src/ECommerce/Libraries/ECommerce.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevSkill.Data;
+using ECommerce.Infrastructure.DbContexts;
+using ECommerce.Infrastructure.Entities;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category, Guid, ApplicationDbContext>, ICategoryRepository
+    {
+        public CategoryRepository(IApplicationDbContext applicationDbContext)
+            : base((ApplicationDbContext)applicationDbContext)
+        {
+
+        }
+    }
+}
---
+++ src/ECommerce/Libraries/ECommerce.Infrastructure/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevSkill.Data;
+using ECommerce.Infrastructure.DbContexts;
+using ECommerce.Infrastructure.Entities;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public interface ICategoryRepository : IRepository<Category, Guid, ApplicationDbContext>
+    {
+    }
+}
