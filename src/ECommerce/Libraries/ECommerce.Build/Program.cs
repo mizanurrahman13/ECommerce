229edb8264c44e0465acbdad4301494df740199a
mizanurrahman13 <mizan.441980@gmail.com>
2022-11-27 20:42:04 +0600

    CONFIG: ECommerce.Build Project Added for .NET Build YAML Override

---
+++ src/ECommerce/Libraries/ECommerce.Build/Program.cs
@@ -0,0 +1,70 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "ECommerce Build",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "develop" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "develop" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Installing .NET",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "6.0"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project(s)"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
+
+var adotNetClient = new ADotNetClient();
+adotNetClient.SerializeAndWriteToFile(
+    githubPipeline,
+    path: "../../../../../.github/workflows/dotnet.yml");
+
+Console.WriteLine("Hello Lamia");
