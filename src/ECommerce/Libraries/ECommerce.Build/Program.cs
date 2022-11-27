b0cb14e3ad33e73d7bb2816e082162ca8caed19e
mizanurrahman13 <mizan.441980@gmail.com>
2022-11-27 21:50:33 +0600

    github folder moved to outer layer

--- src/ECommerce/Libraries/ECommerce.Build/Program.cs
+++ src/ECommerce/Libraries/ECommerce.Build/Program.cs
@@ -1,70 +1,70 @@
-﻿using ADotNet.Clients;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
-using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+﻿//using ADotNet.Clients;
+//using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+//using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+//using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
 
-var githubPipeline = new GithubPipeline
-{
-    Name = "ECommerce Build",
+//var githubPipeline = new GithubPipeline
+//{
+//    Name = "ECommerce Build",
 
-    OnEvents = new Events
-    {
-        Push = new PushEvent
-        {
-            Branches = new string[] { "develop" }
-        },
+//    OnEvents = new Events
+//    {
+//        Push = new PushEvent
+//        {
+//            Branches = new string[] { "develop" }
+//        },
 
-        PullRequest = new PullRequestEvent
-        {
-            Branches = new string[] { "develop" }
-        }
-    },
+//        PullRequest = new PullRequestEvent
+//        {
+//            Branches = new string[] { "develop" }
+//        }
+//    },
 
-    Jobs = new Jobs
-    {
-        Build = new BuildJob
-        {
-            RunsOn = BuildMachines.Windows2022,
+//    Jobs = new Jobs
+//    {
+//        Build = new BuildJob
+//        {
+//            RunsOn = BuildMachines.Windows2022,
 
-            Steps = new List<GithubTask>
-            {
-                new CheckoutTaskV2
-                {
-                    Name = "Checking out Code"
-                },
+//            Steps = new List<GithubTask>
+//            {
+//                new CheckoutTaskV2
+//                {
+//                    Name = "Checking out Code"
+//                },
 
-                new SetupDotNetTaskV1
-                {
-                    Name = "Installing .NET",
+//                new SetupDotNetTaskV1
+//                {
+//                    Name = "Installing .NET",
 
-                    TargetDotNetVersion = new TargetDotNetVersion
-                    {
-                        DotNetVersion = "6.0.x"
-                    }
-                },
+//                    TargetDotNetVersion = new TargetDotNetVersion
+//                    {
+//                        DotNetVersion = "6.0.x"
+//                    }
+//                },
 
-                new RestoreTask
-                {
-                    Name = "Restoring Packages"
-                },
+//                new RestoreTask
+//                {
+//                    Name = "Restoring Packages"
+//                },
 
-                new DotNetBuildTask
-                {
-                    Name = "Building Project(s)"
-                },
+//                new DotNetBuildTask
+//                {
+//                    Name = "Building Project(s)"
+//                },
 
-                new TestTask
-                {
-                    Name = "Running Tests"
-                }
-            }
-        }
-    }
-};
+//                new TestTask
+//                {
+//                    Name = "Running Tests"
+//                }
+//            }
+//        }
+//    }
+//};
 
-var adotNetClient = new ADotNetClient();
-adotNetClient.SerializeAndWriteToFile(
-    githubPipeline,
-    path: "../../../../../.github/workflows/dotnet.yml");
+//var adotNetClient = new ADotNetClient();
+//adotNetClient.SerializeAndWriteToFile(
+//    githubPipeline,
+//    path: "../../../../../../../.github/workflows/dotnet.yml");
 
 Console.WriteLine("Hello Lamia");
